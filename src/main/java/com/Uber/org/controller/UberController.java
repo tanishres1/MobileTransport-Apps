f5f25f839fac8bad5b6b584a26ec00a6f0435b3d
shivani-tanish <yesebu.vani09@gmail.com>
2025-04-10 17:11:56 +0530

    Added Uber Controller api with get msg

---
+++ src/main/java/com/Uber/org/controller/UberController.java
@@ -0,0 +1,12 @@
+package com.Uber.org.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UberController {
+    @GetMapping("/msg")
+    public String getUberMsg(){
+        return "Uber notification get activated";
+    }
+}
