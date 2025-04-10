c3a9c76aa048137a9d10ad5cb1f9b16ddbec71a5
shivani-tanish <yesebu.vani09@gmail.com>
2025-04-10 15:37:25 +0530

    Initial commit

---
+++ src/test/java/com/zomato/org/ZomatoApp/ZomatoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zomato.org.ZomatoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
