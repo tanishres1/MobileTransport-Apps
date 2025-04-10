7f2935f09b02fe222b88c528e691b73c66fef7de
shivani-tanish <yesebu.vani09@gmail.com>
2025-04-10 16:57:57 +0530

    Uber projeect to push to main branch with Mobile Transport repo

---
+++ src/test/java/com/Uber/org/UberAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Uber.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
