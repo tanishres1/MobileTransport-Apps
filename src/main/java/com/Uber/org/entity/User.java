6e2a25e62f2475b99f065b83be63606468f9a620
shivani-tanish <yesebu.vani09@gmail.com>
2025-04-11 18:47:05 +0530

    Added Rider entity class

---
+++ src/main/java/com/Uber/org/entity/User.java
@@ -0,0 +1,23 @@
+package com.Uber.org.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String name;
+}
